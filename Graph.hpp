64e4d756609bf5b199ffbed5fdf207e841cf4f1f
Hansen1030 <hl58@illinois.edu>
2023-02-10 16:20:32 -0600

    hf

---
+++ Graph.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+#include <random>
+
+class Graph {
+    public:
+        Graph() = default;
+        Graph(int num_verticies, int d, int random_type);
+        int[] findSP();
+        int[] findPivot(int[] shortest_path);
+        double findDis(int[] pivots);
+        ~Graph();
+
+    private:
+        int[][] edge_matrix;
+        int d;
+};
+
