7903c52deca57a99beb3d0b3e2ac7038ce9cc101
yyang1729 <47253815+yyang1729@users.noreply.github.com>
2023-02-14 17:35:45 -0600

    combine files

--- Graph.hpp
+++ Graph.hpp
@@ -2,22 +2,28 @@
 
 #include <iostream>
 #include <random>
+#include <vector>
+#include <cstdlib>
+
+using std::vector;
 
 class Graph {
     public:
         Graph() = default;
-        Graph(int num_verticies, int d, int random_type);
-
-        // Graph algorithm to find shortest path from vertex a to vertex b
-        int[] findSP(int start, int end);
-
-        int[] findPivot(int[] shortest_path);
-        double findDis(int[] pivots);
+        //      random_type               random_index_1              random_index_2
+        //  0     uniform                  lower bound                 upper bound
+        //  1      normal                      mean                        std
+        //  2     expnential                   mean                         \
+        //  3      binomial                   times                    probability
+        Graph(int num_verticies, int d, int random_type, double random_index_1, double random_index_2);
+        void reassign_edge(int random_type, double random_index_1, double random_index_2);
+        int * findSP();
+        int * findPivot(int shortest_path[]);
+        double findDis(int pivots[]);
         ~Graph();
 
     private:
-        int[][] edge_matrix;
-        int d;
+        vector< vector<double> > edge_matrix;
+        double random_num_gen(int random_type, double random_index_1, double random_index_2);
 };
-
 
