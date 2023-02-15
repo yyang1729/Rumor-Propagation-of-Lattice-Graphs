44d908447205589d0eacea2478690cb3b40cdb04
Hansen1030 <hl58@illinois.edu>
2023-02-14 22:42:39 -0600

    head file

---
+++ Graph.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <iostream>
+#include <random>
+#include <vector>
+#include <cstdlib>
+
+using std::vector;
+
+class Graph {
+    public:
+        Graph() = default;
+        //      random_type               random_index_1              random_index_2
+        //  0     uniform                  lower bound                 upper bound
+        //  1      normal                      mean                        std
+        //  2     expnential                   mean                         \
+        //  3      binomial                   times                    probability
+        Graph(int num_verticies, int d, int random_type, double random_index_1, double random_index_2);
+        void reassign_edge(int random_type, double random_index_1, double random_index_2);
+        int * findSP();
+        int * findNRP(int shortest_path[]);
+        //double findDis(int pivots[]);
+        ~Graph();
+
+    private:
+        vector< vector<double> > edge_matrix;
+        double random_num_gen(int random_type, double random_index_1, double random_index_2);
+        int d_ = 0;
+};
+
