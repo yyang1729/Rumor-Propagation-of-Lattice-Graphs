94e971fa61b0c034df78f3730fcafd5267186d06
Yongzheng Yang <yyang1729@gmail.com>
2023-03-03 13:49:30 -0600

    write automated data processor

--- pathProcessing.cpp
+++ pathProcessing.cpp
@@ -1,28 +1,53 @@
-#include <fstream>
 #include <iostream>
-#include <sstream>
+#include <fstream>
 #include <string>
-#include <filesystem>
+#include <sys/types.h>
+#include <dirent.h>
+#include <cstring>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    string filename = argv[1];
-    string line;
-    ifstream infile(filename);
-    double total = 0;
-    int N = 0;
-    if (infile.is_open())
-    {
-        while (getline(infile, line))
-        {
-            N++;
-            total += stod(line);
-        }
+    DIR* datadir = opendir("ndata");
+    dirent* entry = readdir(datadir);
+
+    while(entry){
+        char part[6];
         
-        double output = total / N;
-        cout << filename << " : " << output << endl;
+        memcpy(part, entry->d_name, 6);
+        part[6] = 0;
+        
+        if(strcmp(part, "path_N") == 0){
+            char * temp = entry->d_name + 7;
+            int N = atoi(temp);
+            
+            while(temp && temp[0] != 'D'){
+                temp++;
+            }
+
+            if(!temp) {
+                printf("filename did not include D");
+                exit(1);
+            } 
+            int d = atoi(temp + 2);
+
+            int samples = 0;
+            double tottime = 0;
+
+            string line;
+            string filename = entry->d_name;
+
+            ifstream infile("ndata/" + filename);
+            if (infile.is_open()) {
+                while(getline(infile, line)) {
+                    tottime = stod(line);
+                    samples++;
+                }
+            } else {printf("failed to open %s\n", entry->d_name);}
+
+            printf("%d, %d, %f\n", N, d, tottime / samples);
+        }
+        entry = readdir(datadir);
     }
-    return 0;
 }
