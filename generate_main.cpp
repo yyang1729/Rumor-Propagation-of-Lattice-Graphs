c2a1767007b7d7052030e0f196fe650600c322f2
Tianyi Huang <tianyih6@illinois.edu>
2023-03-23 12:57:21 -0500

    Add Apex data

--- generate_main.cpp
+++ generate_main.cpp
@@ -6,49 +6,49 @@
 #include <cmath>
 
 int main(){
-    int num_of_simu = 3;
+    int num_of_simu = 400;
     int x=0;
     int y=0;
     //std::cout<<"start"<<std::endl;
     for(int s = 0; s < 4; s++){
         if(s == 0){ 
-            x = 1000;
+            x = 10000;
         } else if (s == 1){
-            x = 1500;
+            x = 5000;
         } else if (s == 2){
             x = 2000;
         } else {
-            x = 5000;
+            x = 1000;
         }
         //std::cout<<x<<std::endl;
-        for(int t=1; t<x; t++){
-            //if(t == 0){
-            //    y = 3;
-            //} else if (t == 1){
-            //y = std::pow(x, 0.4);
-            //} else if (t == 2){
-            //    y = x/20;
-            //} else if (t == 3){
-            //    y = sqrt(x/2);
-            //} else {
-            //    y = x/5;
-            //}
-            y = t;
-            if (y > x) {
-                std::cout<<"Overflow y greater than x"<<std::endl;
+        for(int t=0; t<4; t++){
+            if(t == 0){
+                y = 4;
+            } else if (t == 1){
+                y = 5;
+            } else if (t == 2){
+                y = 6;
+            } else if (t == 3){
+                y = 7;
+            } else {
+                y = x/5;
             }
+            //y = t;
+            //if (y > x) {
+            //    std::cout<<"Overflow y greater than x"<<std::endl;
+            //}
             //std::cout<<y<<std::endl;
             Graph g = Graph(x,y,0,0,1);  
             //std::cout<<"g"<<std::endl;          
             std::string name = "_N_" + std::to_string(x) + "_D_"+std::to_string(y);
             std::ofstream pathseqfile;
-            std::string pathseqname = "./Fulldata/" + std::to_string(x) + "/path_seq"+name+".csv";
+            std::string pathseqname = "./Apexdata/path_seq"+name+".csv";
             pathseqfile.open(pathseqname);
             std::ofstream pathfile;
-            std::string pathname = "./Fulldata/" + std::to_string(x) + "/path"+name+".csv";
+            std::string pathname = "./Apexdata/path"+name+".csv";
             pathfile.open(pathname);
             std::ofstream nrpfile;
-            std::string nrpname = "./Fulldata/" + std::to_string(x) + "/nrp"+name+".csv";
+            std::string nrpname = "./Apexdata/nrp"+name+".csv";
             nrpfile.open(nrpname);
             for(int u = 0; u < num_of_simu; u++){
                 g.reassign_edge(0,0,1);
