79d6265bf5a391113c65a2635a7f0701b7904a3d
Hansen1030 <hl58@illinois.edu>
2023-03-29 10:45:00 -0500

    gmc

--- generate_main.cpp
+++ generate_main.cpp
@@ -6,32 +6,33 @@
 #include <cmath>
 
 int main(){
-    int num_of_simu = 100;
+    int num_of_simu = 10;
     int x=0;
     int y=0;
     //std::cout<<"start"<<std::endl;
     for(int s = 0; s < 4; s++){
         if(s == 0){ 
-            x = 100;
+            x = 1000;
         } else if (s == 1){
-            x = 15000;
+            x = 5000;
         } else if (s == 2){
-            x = 50000;
+            x = 10000;
         } else {
-            x = 100000;
+            x = 50000;
         }
+        double tmp = sqrt(x);
         //std::cout<<x<<std::endl;
-        for(int t=0; t<4; t++){
+        for(int t=0; t<5; t++){
             if(t == 0){
-                y = 4;
+                y = (int) (0.5 * tmp);
             } else if (t == 1){
-                y = 5;
+                y = (int) (0.7 * tmp);
             } else if (t == 2){
-                y = 6;
+                y = (int) (1.0 * tmp);
             } else if (t == 3){
-                y = 7;
+                y = (int) (1.2 * tmp);
             } else {
-                y = x/5;
+                y = (int) (1.5 * tmp);
             }
             //y = t;
             //if (y > x) {
@@ -41,25 +42,25 @@ int main(){
             Graph g = Graph(x,y,0,0,1);  
             //std::cout<<"g"<<std::endl;          
             std::string name = "_N_" + std::to_string(x) + "_D_"+std::to_string(y);
-            // std::ofstream pathseqfile;
-            // std::string pathseqname = "./Apexdata/path_seq"+name+".csv";
-            // pathseqfile.open(pathseqname);
-            // std::ofstream pathfile;
-            // std::string pathname = "./Apexdata/path"+name+".csv";
-            // pathfile.open(pathname);
+            std::ofstream pathseqfile;
+            std::string pathseqname = "./Mar28data/path_seq"+name+".csv";
+            pathseqfile.open(pathseqname);
+            std::ofstream pathfile;
+            std::string pathname = "./Mar28data/path"+name+".csv";
+            pathfile.open(pathname);
             std::ofstream nrpfile;
-            std::string nrpname = "./more_nrp_data/nrp"+name+".csv";
+            std::string nrpname = "./Mar28data/nrp"+name+".csv";
             nrpfile.open(nrpname);
             for(int u = 0; u < num_of_simu; u++){
                 g.reassign_edge(0,0,1);
                 std::cout<<name+" "+std::to_string(u)+" time"<<std::endl;
                 pair<vector<int>, double> path_result = g.findShortestPath(1,x-1);
                 vector<int> path = path_result.first;
-                // std::string path_str = "";
-                // for(int i : path){
-                //     path_str += std::to_string(i) + ",";
-                // }
-                // path_str += "\n";
+                std::string path_str = "";
+                for(int i : path){
+                    path_str += std::to_string(i) + ",";
+                }
+                path_str += "\n";
                 std::vector<int> nrp =  g.findNRP(path);
                 std::string nrp_str = "";
                 for(int i : nrp){
@@ -67,12 +68,12 @@ int main(){
                 }      
                 nrp_str += "\n";  
 
-                // pathseqfile << path_str;
-                // pathfile << std::to_string(path_result.second)+"\n";
+                pathseqfile << path_str;
+                pathfile << std::to_string(path_result.second)+"\n";
                 nrpfile << nrp_str;
             }
-            // pathseqfile.close();
-            // pathfile.close();
+            pathseqfile.close();
+            pathfile.close();
             nrpfile.close();
         }
     }
