b9986307a34d64478861d09e7b41b51296f78028
Tianyi Huang <tianyih6@illinois.edu>
2023-04-03 01:44:38 -0500

    add 10^4 and 5k data in the /data folder

--- generate_main.cpp
+++ generate_main.cpp
@@ -6,7 +6,7 @@
 #include <cmath>
 
 int main(){
-    int num_of_simu = 900;
+    int num_of_simu = 1000;
     int x=0;
     int y=0;
     //std::cout<<"start"<<std::endl;
@@ -23,11 +23,11 @@ int main(){
         //std::cout<<x<<std::endl;
         for(int t=0; t<1; t++){
             if(t == 0){
-                y = 5;
+                y = std::pow(x, 0.5);
             } else if (t == 1){
                 y = x/10;
             } else if (t == 2){
-                y = std::pow(x, 0.46);
+                y = std::pow(x, 0.5);
             } else if (t == 3){
                 y = std::pow(x, 0.48);
             } else {
@@ -42,13 +42,13 @@ int main(){
             //std::cout<<"g"<<std::endl;          
             std::string name = "_N_" + std::to_string(x) + "_D_"+std::to_string(y);
             std::ofstream pathseqfile;
-            std::string pathseqname = "./Apexdata/path_seq"+name+".csv";
+            std::string pathseqname = "./data/path_seq"+name+".csv";
             pathseqfile.open(pathseqname);
             std::ofstream pathfile;
-            std::string pathname = "./Apexdata/path"+name+".csv";
+            std::string pathname = "./data/path"+name+".csv";
             pathfile.open(pathname);
             std::ofstream nrpfile;
-            std::string nrpname = "./Apexdata/nrp"+name+".csv";
+            std::string nrpname = "./data/nrp"+name+".csv";
             nrpfile.open(nrpname);
             for(int u = 0; u < num_of_simu; u++){
                 g.reassign_edge(0,0,1);
